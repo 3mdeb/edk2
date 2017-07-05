6acacd0f78d80845ecd828c865077ad6c2e7208d
Piotr Król <piotr.krol@3mdeb.com>
2017-07-06 00:26:56 +0200

    apu2-build.sh: initial commit
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

---
+++ apu2-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+build -a IA32 -p CorebootPayloadPkg/CorebootPayloadPkgIa32.dsc -b DEBUG -t GCC5
+cp Build/CorebootPayloadPkgIA32/DEBUG_GCC5/FV/UEFIPAYLOAD.fd ../apu2_fw_rel/apu2/coreboot/
+cd ../apu2_fw_rel
+./apu2/apu2-documentation/scripts/apu2_fw_rel.sh build-ml
+read -n 1 -s -p "Press any key to continue with flashing recent build ..."
+./apu2/apu2-documentation/scripts/apu2_fw_rel.sh flash-ml root@$1
+cd ../edk2
