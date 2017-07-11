dc231f0c34b1e1bd41ab6b7113dd3d1938bdfff5
Piotr Król <piotr.krol@3mdeb.com>
2017-07-11 22:55:34 +0200

    apu2-build.sh: use Ia32X64 instead of Ia32
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- apu2-build.sh
+++ apu2-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-build -a IA32 -p CorebootPayloadPkg/CorebootPayloadPkgIa32.dsc -b DEBUG -t GCC5
-cp Build/CorebootPayloadPkgIA32/DEBUG_GCC5/FV/UEFIPAYLOAD.fd ../apu2_fw_rel/apu2/coreboot/
+# build -a IA32 -p CorebootPayloadPkg/CorebootPayloadPkgIa32.dsc -b DEBUG -t GCC5
+build -a IA32 -a X64 -p CorebootPayloadPkg/CorebootPayloadPkgIa32X64.dsc -b DEBUG -t GCC5
+cp Build/CorebootPayloadPkgX64/DEBUG_GCC5/FV/UEFIPAYLOAD.fd ../apu2_fw_rel/apu2/coreboot/
 cd ../apu2_fw_rel
 ./apu2/apu2-documentation/scripts/apu2_fw_rel.sh build-ml
 read -n 1 -s -p "Press any key to continue with flashing recent build ..."
