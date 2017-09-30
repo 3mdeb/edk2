0e57c9210b2d5970f5d68ca304afdddbaaf85322
Piotr Król <piotr.krol@3mdeb.com>
2017-09-30 17:00:05 +0200

    apu2-build.sh: improve building process
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- apu2-build.sh
+++ apu2-build.sh
@@ -4,6 +4,8 @@ build -a IA32 -a X64 -p CorebootPayloadPkg/CorebootPayloadPkgIa32X64.dsc -b DEBU
 cp Build/CorebootPayloadPkgX64/DEBUG_GCC5/FV/UEFIPAYLOAD.fd ../apu2_fw_rel/apu2/coreboot/
 cd ../apu2_fw_rel
 ./apu2/apu2-documentation/scripts/apu2_fw_rel.sh build-ml
-read -n 1 -s -p "Press any key to continue with flashing recent build ..."
-./apu2/apu2-documentation/scripts/apu2_fw_rel.sh flash-ml root@$1
+cd ../apu-test-suite
+source ~/storage/robot-venv/bin/activate
+robot -v rom_path:$PWD/../apu2_fw_rel/apu2/coreboot/build/ -v host:$1 -v username:root -v password:voyage flash_coreboot_rom.robot
+deactivate
 cd ../edk2
