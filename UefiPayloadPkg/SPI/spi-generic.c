9b862cac5c65425ae0d818f18bcf1100415e5195
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-17 08:39:25 +0200

    Create SPI constructor

---
+++ UefiPayloadPkg/SPI/spi-generic.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <spi-generic.h>
+#include <PiDxe.h>
+
+EFI_STATUS
+EFIAPI
+BlSMMSTOREInitialise (
+  IN EFI_HANDLE         ImageHandle,
+  IN EFI_SYSTEM_TABLE   *SystemTable
+  )
+{
+  return EFI_SUCCESS;
+}
