3529f2f42ac9ade06a38b1b9b3d5f94e80d71434
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-18 08:42:27 +0200

    create spi driver

---
+++ UefiPayloadPkg/SPI/SPI.c
@@ -0,0 +1,19 @@
+/** @file  BlSMMStoreDxe.c
+
+  Copyright (c) 2020, 9elements Agency GmbH<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Include/PiDxe.h>
+
+STATIC
+EFI_STATUS
+InstallSmbusProtocol (
+  IN EFI_HANDLE                        ImageHandle,
+  IN EFI_SYSTEM_TABLE                  *SystemTable
+  )
+{
+  return EFI_SUCCESS;
+}
