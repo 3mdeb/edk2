0c82b547b18971934dae7819a4e3d23c22c4f32b
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-21 14:51:28 +0200

    remove assert 2

--- MdePkg/Include/Library/DebugLib.h
+++ MdePkg/Include/Library/DebugLib.h
@@ -562,7 +562,7 @@ DebugPrintLevelEnabled (
 **/
 #if !defined(MDEPKG_NDEBUG)
   #define CR(Record, TYPE, Field, TestSignature)                                              \
-    (DebugAssertEnabled () && (BASE_CR (Record, TYPE, Field)->Signature != TestSignature)) ?  \
+    BASE_CR (Record, TYPE, Field); //(DebugAssertEnabled () && (BASE_CR (Record, TYPE, Field)->Signature != TestSignature)) ?  \
     (TYPE *) (_ASSERT (CR has Bad Signature), Record) :                                       \
     BASE_CR (Record, TYPE, Field)
 #else
