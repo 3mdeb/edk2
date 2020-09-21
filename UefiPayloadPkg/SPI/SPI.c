fdd65fe6809dfc772594638729f5fccd57f65803
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-21 08:44:43 +0200

    only one definition

--- UefiPayloadPkg/SPI/SPI.c
+++ UefiPayloadPkg/SPI/SPI.c
@@ -13,52 +13,15 @@
 #include "SPI.h"
 
 EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL FvbProtocol = {
-    FvbGetAttributes, // FvbGetAttributes, // GetAttributes
-    FvbGetAttributes, // FvbSetAttributes, // SetAttributes
-    FvbGetAttributes, // FvbGetPhysicalAddress,  // GetPhysicalAddress
-    FvbGetAttributes, // FvbGetBlockSize,  // GetBlockSize
-    FvbGetAttributes, // FvbRead,  // Read
-    FvbGetAttributes, // FvbWrite, // Write
-    FvbGetAttributes, // FvbEraseBlocks, // EraseBlocks
+    FvbGetAttributes, // GetAttributes
+    FvbSetAttributes, // SetAttributes
+    FvbGetPhysicalAddress,  // GetPhysicalAddress
+    FvbGetBlockSize,  // GetBlockSize
+    FvbRead,  // Read
+    FvbWrite, // Write
+    FvbEraseBlocks, // EraseBlocks
     NULL, //ParentHandle
   };
-
-EFI_STATUS
-EFIAPI
-FvbGetAttributes(
-  IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL    *This,
-  OUT       EFI_FVB_ATTRIBUTES_2                   *Attributes
-  )
-{
-  EFI_FVB_ATTRIBUTES_2  FlashFvbAttributes;
-  SMMSTORE_INSTANCE *Instance;
-
-  Instance = INSTANCE_FROM_FVB_THIS(This);
-
-  FlashFvbAttributes = (EFI_FVB_ATTRIBUTES_2) (
-
-      EFI_FVB2_READ_ENABLED_CAP | // Reads may be enabled
-      EFI_FVB2_READ_STATUS      | // Reads are currently enabled
-      EFI_FVB2_STICKY_WRITE     | // A block erase is required to flip bits into EFI_FVB2_ERASE_POLARITY
-      EFI_FVB2_MEMORY_MAPPED    | // It is memory mapped
-      EFI_FVB2_ERASE_POLARITY     // After erasure all bits take this value (i.e. '1')
-
-      );
-
-  // Check if it is write protected
-  if (Instance->Media.ReadOnly != TRUE) {
-
-    FlashFvbAttributes = FlashFvbAttributes         |
-                         EFI_FVB2_WRITE_STATUS      | // Writes are currently enabled
-                         EFI_FVB2_WRITE_ENABLED_CAP;  // Writes may be enabled
-  }
-
-  *Attributes = FlashFvbAttributes;
-
-  DEBUG ((DEBUG_BLKIO, "FvbGetAttributes(0x%X)\n", *Attributes));
-
-  return EFI_SUCCESS;
-}
 
 EFI_STATUS
 EFIAPI
