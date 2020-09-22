ce95917e201635fa42178831d262f54d99bf8c38
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-22 13:10:06 +0200

    add fvb initialization

--- UefiPayloadPkg/SPI/SPI.c
+++ UefiPayloadPkg/SPI/SPI.c
@@ -14,6 +14,54 @@
 #include <Library/HobLib.h>
 #include "SPI.h"
 
+SMMSTORE_INSTANCE  mSMMStoreInstanceTemplate = {
+  SMMSTORE_SIGNATURE, // Signature
+  NULL, // Handle ... NEED TO BE FILLED
+  {
+    0, // MediaId ... NEED TO BE FILLED
+    FALSE, // RemovableMedia
+    TRUE, // MediaPresent
+    FALSE, // LogicalPartition
+    FALSE, // ReadOnly
+    FALSE, // WriteCaching;
+    0, // BlockSize ... NEED TO BE FILLED
+    4, //  IoAlign
+    0, // LastBlock ... NEED TO BE FILLED
+    0, // LowestAlignedLba
+    1, // LogicalBlocksPerPhysicalBlock
+  }, //Media;
+
+  {
+    FvbGetAttributes, // GetAttributes
+    FvbSetAttributes, // SetAttributes
+    FvbGetPhysicalAddress,  // GetPhysicalAddress
+    FvbGetBlockSize,  // GetBlockSize
+    FvbRead,  // Read
+    FvbWrite, // Write
+    FvbEraseBlocks, // EraseBlocks
+    NULL, //ParentHandle
+  }, //  FvbProtoccol;
+  {
+    {
+      {
+        HARDWARE_DEVICE_PATH,
+        HW_VENDOR_DP,
+        {
+          (UINT8)(OFFSET_OF (NOR_FLASH_DEVICE_PATH, End)),
+          (UINT8)(OFFSET_OF (NOR_FLASH_DEVICE_PATH, End) >> 8)
+        }
+      },
+      { 0x0, 0x0, 0x0, { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } }, // GUID ... NEED TO BE FILLED
+    },
+    0, // Index
+    {
+      END_DEVICE_PATH_TYPE,
+      END_ENTIRE_DEVICE_PATH_SUBTYPE,
+      { sizeof (EFI_DEVICE_PATH_PROTOCOL), 0 }
+    }
+    } // DevicePath
+};
+
 EFI_HANDLE Handle = NULL;
 EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL FvbProtocol = {
     FvbGetAttributes, // GetAttributes
@@ -31,8 +79,11 @@ EFI_STATUS EFIAPI SPIInitialize (
   IN EFI_SYSTEM_TABLE                  *SystemTable
   )
 {
+  SMMSTORE_INSTANCE* Instance;
+  Instance = AllocateRuntimeCopyPool (sizeof(SMMSTORE_INSTANCE),&mSMMStoreInstanceTemplate);
   EFI_STATUS Status = EFI_SUCCESS;
   VOID *GuidHob;
+
   GuidHob = GetFirstGuidHob (&gEfiSMMSTOREInfoHobGuid);
   if(GuidHob == NULL) {
     DEBUG((EFI_D_INFO, "GUIDHOB IS NULLLLLL!!!!\n"));
@@ -43,11 +94,11 @@ EFI_STATUS EFIAPI SPIInitialize (
     EFI_NATIVE_INTERFACE,
     &FvbProtocol);
   DEBUG((EFI_D_INFO, "SPI IS HERE\n"));
-    Status = gBS->InstallMultipleProtocolInterfaces (
-                &Handle,
-                &gEfiFirmwareVolumeBlockProtocolGuid, &FvbProtocol,
-                NULL
-                );
+  Status = gBS->InstallMultipleProtocolInterfaces (
+              &Handle,
+              &gEfiFirmwareVolumeBlockProtocolGuid, &FvbProtocol,
+              NULL
+              );
   if(EFI_ERROR (Status)) {
     DEBUG((EFI_D_INFO, "%a Error during protocol installation\n", __FUNCTION__));
   } else {
