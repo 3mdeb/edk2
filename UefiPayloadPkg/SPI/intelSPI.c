2db3334dfe683b8345e16a90833b519438564f77
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-24 14:00:32 +0200

    fix some errors 6

--- UefiPayloadPkg/SPI/intelSPI.c
+++ UefiPayloadPkg/SPI/intelSPI.c
@@ -62,8 +62,68 @@
 #define pci_write_config16 pci_s_write_config16
 #define pci_write_config32 pci_s_write_config32
 
+/* Using a unique datatype for MMIO writes makes the pointers to _not_
+ * qualify for pointer aliasing with any other objects in memory.
+ *
+ * MMIO offset is a value originally derived from 'struct device *'
+ * in ramstage. For the compiler to not discard this MMIO offset value
+ * from CPU registers after any MMIO writes, -fstrict-aliasing has to
+ * be also set for the build.
+ *
+ * Bottom 12 bits (4 KiB) are reserved to address the registers of a
+ * single PCI function. Declare the bank as a union to aVOID some casting
+ * in the functions below.
+ */
+union pci_bank {
+	UINT8 reg8[4096];
+	UINT16 reg16[4096 / sizeof(UINT16)];
+	UINT32 reg32[4096 / sizeof(UINT32)];
+};
+
 typedef unsigned long int	uintptr_t;
 typedef UINT32 pci_devfn_t;
+
+static  __attribute__((always_inline))
+volatile union pci_bank *pcicfg(pci_devfn_t dev)
+{
+	return (VOID *)&pci_mmconf[PCI_DEVFN_OFFSET(dev)];
+}
+
+static  __attribute__((always_inline))
+UINT8 pci_mmio_read_config8(pci_devfn_t dev, UINT16 reg)
+{
+	return pcicfg(dev)->reg8[reg];
+}
+
+static  __attribute__((always_inline))
+UINT16 pci_mmio_read_config16(pci_devfn_t dev, UINT16 reg)
+{
+	return pcicfg(dev)->reg16[reg / sizeof(UINT16)];
+}
+
+static  __attribute__((always_inline))
+UINT32 pci_mmio_read_config32(pci_devfn_t dev, UINT16 reg)
+{
+	return pcicfg(dev)->reg32[reg / sizeof(UINT32)];
+}
+
+static __attribute__((always_inline))
+VOID pci_mmio_write_config8(pci_devfn_t dev, UINT16 reg, UINT8 value)
+{
+	pcicfg(dev)->reg8[reg] = value;
+}
+
+static  __attribute__((always_inline))
+VOID pci_mmio_write_config16(pci_devfn_t dev, UINT16 reg, UINT16 value)
+{
+	pcicfg(dev)->reg16[reg / sizeof(UINT16)] = value;
+}
+
+static  __attribute__((always_inline))
+VOID pci_mmio_write_config32(pci_devfn_t dev, UINT16 reg, UINT32 value)
+{
+	pcicfg(dev)->reg32[reg / sizeof(UINT32)] = value;
+}
 
 static __attribute__((always_inline))
 UINT8 pci_s_read_config8(pci_devfn_t dev, UINT16 reg)
@@ -188,66 +248,6 @@ struct spi_flash_ops {
 /* By not assigning this to CONFIG_MMCONF_BASE_ADDRESS here we
  * prevent some sub-optimal constant folding. */
 extern UINT8 *const pci_mmconf;
-
-/* Using a unique datatype for MMIO writes makes the pointers to _not_
- * qualify for pointer aliasing with any other objects in memory.
- *
- * MMIO offset is a value originally derived from 'struct device *'
- * in ramstage. For the compiler to not discard this MMIO offset value
- * from CPU registers after any MMIO writes, -fstrict-aliasing has to
- * be also set for the build.
- *
- * Bottom 12 bits (4 KiB) are reserved to address the registers of a
- * single PCI function. Declare the bank as a union to aVOID some casting
- * in the functions below.
- */
-union pci_bank {
-	UINT8 reg8[4096];
-	UINT16 reg16[4096 / sizeof(UINT16)];
-	UINT32 reg32[4096 / sizeof(UINT32)];
-};
-
-static  __attribute__((always_inline))
-volatile union pci_bank *pcicfg(pci_devfn_t dev)
-{
-	return (VOID *)&pci_mmconf[PCI_DEVFN_OFFSET(dev)];
-}
-
-static  __attribute__((always_inline))
-UINT8 pci_mmio_read_config8(pci_devfn_t dev, UINT16 reg)
-{
-	return pcicfg(dev)->reg8[reg];
-}
-
-static  __attribute__((always_inline))
-UINT16 pci_mmio_read_config16(pci_devfn_t dev, UINT16 reg)
-{
-	return pcicfg(dev)->reg16[reg / sizeof(UINT16)];
-}
-
-static  __attribute__((always_inline))
-UINT32 pci_mmio_read_config32(pci_devfn_t dev, UINT16 reg)
-{
-	return pcicfg(dev)->reg32[reg / sizeof(UINT32)];
-}
-
-static __attribute__((always_inline))
-VOID pci_mmio_write_config8(pci_devfn_t dev, UINT16 reg, UINT8 value)
-{
-	pcicfg(dev)->reg8[reg] = value;
-}
-
-static  __attribute__((always_inline))
-VOID pci_mmio_write_config16(pci_devfn_t dev, UINT16 reg, UINT16 value)
-{
-	pcicfg(dev)->reg16[reg / sizeof(UINT16)] = value;
-}
-
-static  __attribute__((always_inline))
-VOID pci_mmio_write_config32(pci_devfn_t dev, UINT16 reg, UINT32 value)
-{
-	pcicfg(dev)->reg32[reg / sizeof(UINT32)] = value;
-}
 
 /*
  * The functions pci_mmio_config*_addr provide a way to determine the MMIO address of a PCI
