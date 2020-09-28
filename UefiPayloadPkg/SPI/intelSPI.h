74088372dbb33c4a84a0ca547ca6177ac318fb01
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-28 10:52:34 +0200

    fix redefinitions

--- UefiPayloadPkg/SPI/intelSPI.h
+++ UefiPayloadPkg/SPI/intelSPI.h
@@ -4,23 +4,6 @@
 #ifndef SOUTHBRIDGE_INTEL_SPI_H
 #define SOUTHBRIDGE_INTEL_SPI_H
 
-enum optype {
-	READ_NO_ADDR = 0,
-	WRITE_NO_ADDR = 1,
-	READ_WITH_ADDR = 2,
-	WRITE_WITH_ADDR = 3
-};
-
-struct intel_spi_op {
-	UINT8 op;
-	enum optype type;
-};
-
-struct intel_swseq_spi_config {
-	UINT8 opprefixes[2];
-	struct intel_spi_op ops[8];
-};
-
 void spi_finalize_ops(void);
 void intel_southbridge_override_spi(struct intel_swseq_spi_config *spi_config);
 
