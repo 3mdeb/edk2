00add80990067b7ba7d1fc40b44d51ed436cbbb9
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-29 10:08:52 +0200

    fixes 4

--- UefiPayloadPkg/SPI/lpc.c
+++ UefiPayloadPkg/SPI/lpc.c
@@ -4,11 +4,11 @@
 #include "pci_ops.h"
 #include "utils.h"
 
-UINT32 *lpc_get_spibase(void)
+unsigned long int lpc_get_spibase(void)
 {
 	UINT32 base;
 
 	base = pci_read_config32(_LPCB_DEV, SPIROM_BASE_ADDRESS_REGISTER);
 	base = ALIGN_DOWN(base, SPI_BASE_ALIGNMENT);
-	return (UINT32)base;
+	return (unsigned long int)base;
 }
