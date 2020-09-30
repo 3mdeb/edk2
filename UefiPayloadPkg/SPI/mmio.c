bc79abeda99e4a6c6b75e95f1d5b6e5bb39fdad0
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-30 08:42:26 +0200

    fix

--- UefiPayloadPkg/SPI/mmio.c
+++ UefiPayloadPkg/SPI/mmio.c
@@ -6,32 +6,32 @@
 #include <Include/PiDxe.h>
 #include "mmio.h"
 
-static inline UINT8 read8(CONST VOID *addr)
-{
-	return *(volatile uint8_t *)addr;
-}
+// static inline UINT8 read8(CONST VOID *addr)
+// {
+// 	return *(volatile uint8_t *)addr;
+// }
 
-static inline UINT16 read16(CONST VOID *addr)
-{
-	return *(volatile UINT16 *)addr;
-}
+// static inline UINT16 read16(CONST VOID *addr)
+// {
+// 	return *(volatile UINT16 *)addr;
+// }
 
-static inline UINT32 read32(CONST VOID *addr)
-{
-	return *(volatile UINT32 *)addr;
-}
+// static inline UINT32 read32(CONST VOID *addr)
+// {
+// 	return *(volatile UINT32 *)addr;
+// }
 
-static inline VOID write8(VOID *addr, uint8_t val)
-{
-	*(volatile uint8_t *)addr = val;
-}
+// static inline VOID write8(VOID *addr, uint8_t val)
+// {
+// 	*(volatile uint8_t *)addr = val;
+// }
 
-static inline VOID write16(VOID *addr, UINT16 val)
-{
-	*(volatile UINT16 *)addr = val;
-}
+// static inline VOID write16(VOID *addr, UINT16 val)
+// {
+// 	*(volatile UINT16 *)addr = val;
+// }
 
-static inline VOID write32(VOID *addr, UINT32 val)
-{
-	*(volatile UINT32 *)addr = val;
-}
+// static inline VOID write32(VOID *addr, UINT32 val)
+// {
+// 	*(volatile UINT32 *)addr = val;
+// }
