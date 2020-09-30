734e5cf9df43a799dcf84cf08ea6155b48de0cf1
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-30 09:44:20 +0200

    fix static

--- UefiPayloadPkg/SPI/pci_ops.h
+++ UefiPayloadPkg/SPI/pci_ops.h
@@ -5,12 +5,12 @@
 #include "device.h"
 #include "pci_type.h"
 
-UINT32 pci_s_read_config32(pci_devfn_t dev, UINT16 reg);
-pci_devfn_t pcidev_bdf(const struct device *dev);
+static UINT32 pci_s_read_config32(pci_devfn_t dev, UINT16 reg);
+static pci_devfn_t pcidev_bdf(const struct device *dev);
 VOID pcidev_die(VOID);
-pci_devfn_t pcidev_assert(CONST struct device *dev);
-UINT32 pci_read_config32(CONST struct device *dev, UINT16 reg);
-void pci_s_write_config32(pci_devfn_t dev, UINT16 reg, UINT32 value);
-void pci_write_config32(const struct device *dev, UINT16 reg, UINT32 val);
+static pci_devfn_t pcidev_assert(CONST struct device *dev);
+static UINT32 pci_read_config32(CONST struct device *dev, UINT16 reg);
+static void pci_s_write_config32(pci_devfn_t dev, UINT16 reg, UINT32 value);
+static void pci_write_config32(const struct device *dev, UINT16 reg, UINT32 val);
 
 #endif /* PCI_OPS_H */
