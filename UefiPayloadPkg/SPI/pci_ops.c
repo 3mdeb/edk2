c477d7b7f391bc1222f4cd38139c1d3f45dacda7
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2020-09-30 09:51:06 +0200

    fix definitions

--- UefiPayloadPkg/SPI/pci_ops.c
+++ UefiPayloadPkg/SPI/pci_ops.c
@@ -6,47 +6,47 @@
 #include "device.h"
 #include "pci_mmio_cfg.h"
 
-static __attribute__ ((__always_inline__)) inline
-UINT32 pci_s_read_config32(pci_devfn_t dev, UINT16 reg)
-{
-	return pci_mmio_read_config32(dev, reg);
-}
+// static __attribute__ ((__always_inline__)) inline
+// UINT32 pci_s_read_config32(pci_devfn_t dev, UINT16 reg)
+// {
+// 	return pci_mmio_read_config32(dev, reg);
+// }
 
-static __attribute__ ((__always_inline__)) inline
-pci_devfn_t pcidev_bdf(CONST struct device *dev)
-{
-	return (dev->path.pci.devfn << 12) | (dev->bus->secondary << 20);
-}
+// static __attribute__ ((__always_inline__)) inline
+// pci_devfn_t pcidev_bdf(CONST struct device *dev)
+// {
+// 	return (dev->path.pci.devfn << 12) | (dev->bus->secondary << 20);
+// }
 
-__attribute__ ((noreturn))
-VOID pcidev_die(VOID)
-{
-	die("PCI: dev is NULL!\n");
-	while(1);
-}
+// __attribute__ ((noreturn))
+// VOID pcidev_die(VOID)
+// {
+// 	die("PCI: dev is NULL!\n");
+// 	while(1);
+// }
 
-static __attribute__ ((__always_inline__)) inline
-pci_devfn_t pcidev_assert(CONST struct device *dev)
-{
-	if (!dev)
-		pcidev_die();
-	return pcidev_bdf(dev);
-}
+// static __attribute__ ((__always_inline__)) inline
+// pci_devfn_t pcidev_assert(CONST struct device *dev)
+// {
+// 	if (!dev)
+// 		pcidev_die();
+// 	return pcidev_bdf(dev);
+// }
 
-static __attribute__ ((__always_inline__)) inline
-UINT32 pci_read_config32(CONST struct device *dev, UINT16 reg)
-{
-	return pci_s_read_config32(PCI_BDF(dev), reg);
-}
+// static __attribute__ ((__always_inline__)) inline
+// UINT32 pci_read_config32(CONST struct device *dev, UINT16 reg)
+// {
+// 	return pci_s_read_config32(PCI_BDF(dev), reg);
+// }
 
-static __attribute__ ((__always_inline__)) inline
-VOID pci_s_write_config32(pci_devfn_t dev, UINT16 reg, UINT32 value)
-{
-	pci_mmio_write_config32(dev, reg, value);
-}
+// static __attribute__ ((__always_inline__)) inline
+// VOID pci_s_write_config32(pci_devfn_t dev, UINT16 reg, UINT32 value)
+// {
+// 	pci_mmio_write_config32(dev, reg, value);
+// }
 
-static __attribute__ ((__always_inline__)) inline
-VOID pci_write_config32(CONST struct device *dev, UINT16 reg, UINT32 val)
-{
-	pci_s_write_config32(PCI_BDF(dev), reg, val);
-}
+// static __attribute__ ((__always_inline__)) inline
+// VOID pci_write_config32(CONST struct device *dev, UINT16 reg, UINT32 val)
+// {
+// 	pci_s_write_config32(PCI_BDF(dev), reg, val);
+// }
